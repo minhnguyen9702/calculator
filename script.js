15e2a1e6ff14acb67900d49f0d052883223550b6
minhnguyen9702 <minhnguyen9702@gmail.com>
2022-12-15 20:49:20 +1100

    make numbers appear display when buttons clicked

---
+++ script.js
@@ -0,0 +1,38 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(operator, a, b) {
+    if (operator == "+") {
+        add(a, b);
+    } if (operator == "-") {
+        subtract(a, b);
+    } if (operator == "*") {
+        multiply(a, b);
+    } if (operator == "/") {
+        divide(a, b);
+    }
+};
+
+let displayValue = ""
+const bigDisplay = document.querySelector(".big.display")
+
+const nums = document.querySelectorAll(".num")
+nums.forEach(num => num.addEventListener('click', function(e) {
+    if (num.textContent != 0 || displayValue != "") {
+        displayValue = displayValue + num.textContent
+        bigDisplay.textContent = displayValue
+    }
+}));
