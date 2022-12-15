991ca39678b7e733d2d4903733de2b58be0d9b64
minhnguyen9702 <minhnguyen9702@gmail.com>
2022-12-15 21:15:41 +1100

    implement clear function

--- script.js
+++ script.js
@@ -27,7 +27,23 @@ function operate(operator, a, b) {
 };
 
 let displayValue = ""
+let operator = ""
+let firstNumber = ""
+let secondNumber = ""
+
+const clearNum = document.querySelector("#clear")
+clearNum.addEventListener('click', function(e) {
+    displayValue = ""
+    operator = ""
+    firstNumber = ""
+    secondNumber = ""
+    bigDisplay.textContent = displayValue
+    smallDisplay.textContent = displayValue
+});
+
 const bigDisplay = document.querySelector(".big.display")
+const smallDisplay = document.querySelector(".small.display")
+
 
 const nums = document.querySelectorAll(".num")
 nums.forEach(num => num.addEventListener('click', function(e) {
@@ -36,3 +52,16 @@ nums.forEach(num => num.addEventListener('click', function(e) {
         bigDisplay.textContent = displayValue
     }
 }));
+
+const ops = document.querySelectorAll(".op")
+ops.forEach(op => op.addEventListener('click', function(e) {
+    if (firstNumber == "") {
+        firstNumber = displayValue
+        displayValue = ""
+        bigDisplay.textContent = displayValue
+        operator = op.textContent
+        smallDisplay.textContent = firstNumber + operator
+    }
+}));
+
+const equals = document.querySelectorAll("#equals")
